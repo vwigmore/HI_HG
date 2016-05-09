72acbd55701e2c2a7f798035956115289ba4c027
codesalad <nguyen094@gmail.com>
2016-05-09 16:11:19 +0200

    fixed hands to arms

---
+++ MarketSim/Assets/src/controllers/LeftHandController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftHandController : MonoBehaviour {
+
+    private GameObject root;
+    private GameObject hand;
+
+	// Use this for initialization
+	void Start () {
+        this.hand = GameObject.Find("Manus_Handv2_Left");
+        this.root = GameObject.Find("left_hand");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newpos = this.root.transform.position;
+        this.hand.transform.position = newpos;
+        Debug.Log(this.root.transform.rotation);
+        this.hand.transform.rotation = this.root.transform.rotation;
+        this.hand.transform.Rotate(Vector3.up, -90);
+        this.hand.transform.Rotate(Vector3.forward, -90);
+	}
+}
---
+++ MarketSim/Assets/src/controllers/RightHandController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightHandController : MonoBehaviour
+{
+
+    private GameObject root;
+    private GameObject hand;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.hand = GameObject.Find("Manus_Handv2_Right");
+        this.root = GameObject.Find("right_hand");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newpos = this.root.transform.position;
+        this.hand.transform.position = newpos;
+        Debug.Log(this.root.transform.rotation);
+        this.hand.transform.rotation = this.root.transform.rotation;
+        this.hand.transform.Rotate(Vector3.up, 90);
+        this.hand.transform.Rotate(Vector3.forward, -90);
+    }
+}
