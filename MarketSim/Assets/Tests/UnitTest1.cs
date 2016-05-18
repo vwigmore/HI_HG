02d4960bab450120fb112c5ce7b517e32ec2f453
Viktor Wigmore <v.wigmore@student.tudelft.nl>
2016-05-18 12:45:20 +0200

    trying to write initial test

---
+++ MarketSim/Assets/Tests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using Assets.src.model;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+
+[TestFixture]
+public class UnitTest1
+{
+    #region Fields
+
+    private ManusGrab ms;
+
+    #endregion Fields
+
+    #region Methods
+
+    [SetUp]
+    public void Setup()
+    {
+        ms = null;
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        ms = null;
+    }
+
+    [Test]
+    public void TestMethod()
+    {
+        Assert.AreEqual(6, 5 + 1);
+    }
+
+    #endregion Methods
+}
