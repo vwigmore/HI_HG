7e92959f6743d491c40d23af83b19b9e674b8ce1
Magdalena <M.Simidzioski-1@student.tudelft.nl>
2016-05-25 16:37:06 +0200

    comments added :)

--- MarketSim/Assets/Basket.cs
+++ MarketSim/Assets/Basket.cs
@@ -1,33 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Translate the handle to the basket
+/// </summary>
+/// <seealso cref="UnityEngine.MonoBehaviour" />
 public class Basket : MonoBehaviour
 {
     #region Fields
 
+
+    /// <summary>
+    /// The basket
+    /// </summary>
     protected GameObject basket;
-
+    /// <summary>
+    /// The handle
+    /// </summary>
     protected GameObject handle;
-
+    /// <summary>
+    /// The handle model
+    /// </summary>
     protected GameObject handleModel;
-
+    /// <summary>
+    /// The basket model
+    /// </summary>
     protected GameObject basketModel;
 
     #endregion Fields
 
     #region Methods
     // Use this for initialization
+    /// <summary>
+    /// Starts this instance
+    /// </summary>
     void Start()
     {
-        ///this.handle = Resources.Load<GameObject>("single_basket_handle");
-        //this.basket = Resources.Load<GameObject>("single_basket_no_handle");
         this.basketModel = GameObject.Find("single_basket_no_handle");
         this.handleModel = GameObject.Find("single_basket_handle");
 
     }
 
     /// <summary>
-    /// Update position.
+    /// Update position of basket and handle
     /// </summary>
     void Update()
     {
