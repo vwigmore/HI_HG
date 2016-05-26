a52d24f3b3285133f19d73bee16fd23079b3b6db
Magdalena <M.Simidzioski-1@student.tudelft.nl>
2016-05-26 15:39:55 +0200

    Player Controller refactored  #21

--- MarketSim/Assets/src/model/Player.cs
+++ MarketSim/Assets/src/model/Player.cs
@@ -1,37 +1,99 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// 
+/// </summary>
 public class Player
 {
     #region Fields
 
+    /// <summary>
+    /// The mouse sensitivity
+    /// </summary>
     public float mouseSensitivity = 3.0f;
+    /// <summary>
+    /// The move speed
+    /// </summary>
     public float moveSpeed = 3.0f;
+    /// <summary>
+    /// The jump speed
+    /// </summary>
     public float jumpSpeed = 5.0f;
+    /// <summary>
+    /// The gravity
+    /// </summary>
     public float gravity = 20f;
+    /// <summary>
+    /// Character controller for the player
+    /// </summary>
     private CharacterController pc;
+    /// <summary>
+    /// The move direction
+    /// </summary>
     private Vector3 moveDirection;
+    /// <summary>
+    /// The player height
+    /// </summary>
     private float playerHeight;
+    /// <summary>
+    /// The crouch height
+    /// </summary>
     private float crouchHeight;
 
+    /// <summary>
+    /// The cam_vert_rot
+    /// </summary>
     private float cam_vert_rot = 0;
+    /// <summary>
+    /// The cam_cap
+    /// </summary>
     private float cam_cap = 90;
 
+    /// <summary>
+    /// The mouse axis y
+    /// </summary>
     private float mouseAxisY;
+    /// <summary>
+    /// The mouse axis x
+    /// </summary>
     private float mouseAxisX;
 
+    /// <summary>
+    /// The v axis
+    /// </summary>
     private float vAxis;
+    /// <summary>
+    /// The h axis
+    /// </summary>
     private float hAxis;
 
+    /// <summary>
+    /// The hip
+    /// </summary>
     private GameObject hip, leftFoot, rightFoot;
+    /// <summary>
+    /// The last delta height
+    /// </summary>
     private float lastDeltaHeight, initialDeltaHeight;
 
+    /// <summary>
+    /// The model
+    /// </summary>
     private GameObject model;
 
 
     #endregion Fields
 
     #region Constructors
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Player"/> class.
+    /// </summary>
+    /// <param name="player">The player.</param>
+    /// <param name="model">The model.</param>
+    /// <param name="hip">The hip.</param>
+    /// <param name="leftFoot">The left foot.</param>
+    /// <param name="rightFoot">The right foot.</param>
     public Player(CharacterController player, GameObject model, GameObject hip, GameObject leftFoot, GameObject rightFoot)
     {
         pc = player;
@@ -44,6 +106,9 @@ public class Player
 
     #region Methods
 
+    /// <summary>
+    /// Updates the crouch.
+    /// </summary>
     public void UpdateCrouch()
     {
         float minFootY = Mathf.Min(this.leftFoot.transform.position.y, this.rightFoot.transform.position.y);
@@ -64,6 +129,9 @@ public class Player
         lastDeltaHeight = deltaHeight;
     }
 
+    /// <summary>
+    /// Updates the rotation.
+    /// </summary>
     public void updateRotation()
     {
         mouseAxisY = Input.GetAxis("Mouse Y");
@@ -78,7 +146,24 @@ public class Player
         pc.transform.Rotate(rotAngles);
     }
 
+    /// <summary>
+    /// Caps the vector.
+    /// </summary>
+    /// <param name="vec">The vec.</param>
+    /// <param name="max">The maximum.</param>
+    /// <param name="min">The minimum.</param>
+    /// <returns></returns>
+    public Vector3 capVector(Vector3 vec, float max, float min)
+    {
+        vec.x = Mathf.Min(Mathf.Max(vec.x, min), max);
+        vec.z = Mathf.Min(Mathf.Max(vec.z, min), max);
+        return vec;
+    }
 
+
+    /// <summary>
+    /// Updates the movement.
+    /// </summary>
     public void updateMovement()
     {
         vAxis = Input.GetAxis("Vertical");
@@ -94,20 +179,9 @@ public class Player
 
             // Multiplies vector with speed (axes are between -1 and 1, not much).
             this.moveDirection *= moveSpeed;
-            if (Input.GetButton("Jump"))
-
-                this.moveDirection.y = jumpSpeed;
-
+           
             // Capping move speed: moveDirection hypotenuse
-            if (this.moveDirection.x < -moveSpeed)
-                this.moveDirection.x = -moveSpeed;
-            else if (this.moveDirection.x > moveSpeed)
-                this.moveDirection.x = moveSpeed;
-
-            if (this.moveDirection.z < -moveSpeed)
-                this.moveDirection.z = -moveSpeed;
-            else if (this.moveDirection.z > moveSpeed)
-                this.moveDirection.z = moveSpeed;
+            capVector(moveDirection, moveSpeed, -moveSpeed);
         }
         else
         {
