80e072e36e6b8a5b3c47b3d354987e6b376c9e4c
Magdalena <M.Simidzioski-1@student.tudelft.nl>
2016-05-27 13:10:51 +0200

    comments added in PlayerTest

--- MarketSim/Assets/Editor/PlayerTest.cs
+++ MarketSim/Assets/Editor/PlayerTest.cs
@@ -4,29 +4,76 @@ using System.Collections;
 using UnityEditor;
 using UnityEngine;
 
+/// <summary>
+/// Tests for the Player class
+/// </summary>
 [TestFixture]
 public class PlayerTest
 {
     #region Fields
 
+    /// <summary>
+    /// Character controller for the player
+    /// </summary>
     CharacterController pc;
+
+    /// <summary>
+    /// The model
+    /// </summary>
     GameObject model;
+
+    /// <summary>
+    /// The hip
+    /// </summary>
     GameObject hip;
+
+    /// <summary>
+    /// The left foot
+    /// </summary>
     GameObject leftFoot;
+
+    /// <summary>
+    /// The right foot
+    /// </summary>
     GameObject rightFoot;
 
+    /// <summary>
+    /// Coordinates for vector position1
+    /// </summary>
     float x1 = -0.1f, y1 = 0.0f, z1 = -17.4f;
+
+    /// <summary>
+    /// Coordinates for vector position2
+    /// </summary>
     float x2 = -0.2f, y2 = 0.0f, z2 = -17.1f;
+
+    /// <summary>
+    /// Coordinates for vector position3
+    /// </summary>
     float x3 = 0.1f, y3 = 0.0f, z3 = -17.1f;
+
+    /// <summary>
+    /// Coordinates for vector capVector1
+    /// </summary>
     float x4 = 1.0f, y4 = 2.0f, z4 = 3.0f;
+
+    /// <summary>
+    /// Coordinates for vector capVector2
+    /// </summary>
     float x5 = 10.0f, y5 = 4.0f, z5 = 8.0f;
 
+    /// <summary>
+    /// The player
+    /// </summary>
     Player player;
 
     #endregion Fields
 
     #region Methods
 
+    /// <summary>
+    /// Setups this instance.
+    /// </summary>
     [SetUp]
     public void Setup()
     {
@@ -47,18 +94,27 @@ public class PlayerTest
 
     }
 
+    /// <summary>
+    /// Tears down.
+    /// </summary>
     [TearDown]
     public void TearDown()
     {
         player = null;
     }
 
+    /// <summary>
+    /// Test for the constructor. 
+    /// </summary>
     [Test]
     public void TestNull()
     {
         Assert.IsNotNull(player);
     }
 
+    /// <summary>
+    /// Test for the UpdateCrouch method
+    /// </summary>
     [Test]
     public void UpdateCrouchTestModel()
     {
@@ -68,6 +124,9 @@ public class PlayerTest
         Assert.AreEqual(player.model.transform.position.ToString(), position1.ToString());
     }
 
+    /// <summary>
+    /// Test for the UpdateCrouch method
+    /// </summary>
     [Test]
     public void UpdateCrouchTestLeftFoot()
     {
@@ -77,6 +136,9 @@ public class PlayerTest
         Assert.AreEqual(player.leftFoot.transform.position.ToString(), position2.ToString());
     }
 
+    /// <summary>
+    /// Test for the UpdateCrouch method
+    /// </summary>
     [Test]
     public void UpdateCrouchTestrightFoot()
     {
@@ -85,25 +147,34 @@ public class PlayerTest
         player.UpdateCrouch();
         Assert.AreEqual(player.rightFoot.transform.position.ToString(), position3.ToString());
     }
-   
+
+    /// <summary>
+    /// Test for the CapVector method
+    /// </summary>
     [Test]
     public void capVectorTest1()
     {
-        Vector3 expected1 = new Vector3(x4, y4, z4);
+        Vector3 capVector1 = new Vector3(x4, y4, z4);
         Vector3 vec1 = new Vector3(1,2,3);
         player.capVector(vec1, 3, 6);
-        Assert.AreEqual(vec1.ToString(), expected1.ToString());
+        Assert.AreEqual(vec1.ToString(), capVector1.ToString());
     }
 
+    /// <summary>
+    /// Test for the CapVector method
+    /// </summary>
     [Test]
     public void capVectorTest2()
     {
-        Vector3 expected2 = new Vector3(x5, y5, z5);
+        Vector3 capVector2 = new Vector3(x5, y5, z5);
         Vector3 vec2 = new Vector3(10, 4, 8);
         player.capVector(vec2, 4, 8);
-        Assert.AreEqual(vec2.ToString(), expected2.ToString());
+        Assert.AreEqual(vec2.ToString(), capVector2.ToString());
     }
 
+    /// <summary>
+    /// Test for the UpdateMovement method
+    /// </summary>
     [Test]
     public void UpdateMovementTest()
     {
@@ -113,6 +184,9 @@ public class PlayerTest
         Assert.AreEqual(player.pc.transform.position.ToString(), newPos.ToString());
     }
 
+    /// <summary>
+    /// Test for the UpdateRotation method
+    /// </summary>
     [Test]
     public void UpdateRotationTest()
     {
