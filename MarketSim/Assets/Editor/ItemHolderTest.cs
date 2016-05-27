82c55f9a9d63ff5d8afcafebb95205e5243e0ce4
MKlaassen <matthijsmas@gmail.com>
2016-05-27 14:09:35 +0200

    added comments

--- MarketSim/Assets/Editor/ItemHolderTest.cs
+++ MarketSim/Assets/Editor/ItemHolderTest.cs
@@ -2,39 +2,62 @@
 using NUnit.Framework;
 using UnityEngine;
 
+/// <summary>
+/// Test for the ItemHolder
+/// </summary>
 [TestFixture]
 public class ItemHolderTest
 {
     #region Fields
 
-    private readonly int four = 4;
-    private readonly int two = 2;
+    /// <summary>
+    /// Number of rows in basket
+    /// </summary>
+    private readonly int row = 4;
+
+    /// <summary>
+    /// Number of columns in basket
+    /// </summary>
+    private readonly int col = 2;
+
+    /// <summary>
+    /// The item h
+    /// </summary>
     private ItemHolder itemH;
 
     #endregion Fields
 
     #region Methods
 
+    /// <summary>
+    /// Setups this instance.
+    /// </summary>
     [SetUp]
     public void Setup()
     {
         GameObject basket = new GameObject("basket");
         basket.AddComponent<Rigidbody>();
         basket.AddComponent<BoxCollider>();
-        itemH = new ItemHolder(basket, two, four);
+        itemH = new ItemHolder(basket, col, row);
     }
 
+    /// <summary>
+    /// Tears down.
+    /// </summary>
     [TearDown]
     public void TearDown()
     {
         itemH = null;
     }
 
+    /// <summary>
+    /// Tests the size of offsets.
+    /// </summary>
     [Test]
     public void TestSizeOfOffsets()
     {
         itemH.InitOffsets();
-        Assert.AreEqual(itemH.offsets.Length, two * four);
+        Assert.AreEqual(itemH.offsets.Length, col * row);
     }
 
     #endregion Methods
