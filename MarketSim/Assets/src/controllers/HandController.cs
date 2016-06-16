0ad6c5ba64dc8c3011276ec62318c5c2f06c7285
Maria Simidzioski <mariasimidzioski@hotmail.com>
2016-06-16 15:06:45 +0200

    finished hand controller, #69

--- MarketSim/Assets/src/controllers/HandController.cs
+++ MarketSim/Assets/src/controllers/HandController.cs
@@ -1,4 +1,4 @@
-using Assets.src.model;
+﻿using Assets.src.model;
 using ManusMachina;
 using System.Collections;
 using UnityEngine;
@@ -41,13 +41,13 @@ public class HandController : MonoBehaviour
         hand = HandFactory.createHand(glove_hand, RootTransform);
         collisionContacts = new ArrayList();
         bends = new bool[5];
-        BendFingersIsTrue();
+        ResetBends();
     }
 
     /// <summary>
     /// Tells that the fingers are bend.
     /// </summary>
-    private void BendFingersIsTrue()
+    private void ResetBends()
     {
         for (int i = 0; i < bends.Length; i++)
             bends[i] = true;
@@ -58,27 +58,10 @@ public class HandController : MonoBehaviour
     /// </summary>
     private void Update()
     {
-        UpdateHandPosition();
-        hand.UpdateHand(bends);
-        hand.UpdateGestures();
-
+        hand.Update(bends);
         ProcessContacts();
-
-        Vector3 contactPoint = CalculateContactPoint();
-        hand.GetManusGrab().UpdateGrabbedObject(contactPoint, hand.GetRootTransform());
+        hand.GetManusGrab().UpdateGrabbedObject(CalculateContactPoint(), hand.GetRootTransform());
         hand.GetManusGrab().basket.UpdateList();
-    }
-
-    /// <summary>
-    /// Updates the position of the hand.
-    /// </summary>
-    private void UpdateHandPosition()
-    {
-
-        if (!Manager.MKBOnly)
-        {
-            hand.UpdatePosition();
-        }
     }
 
     /// <summary>
@@ -89,7 +72,7 @@ public class HandController : MonoBehaviour
     {
         if (collisionContacts.Count == 0)
             return ((Collider)hand.GetColliders()[0]).transform.position;
-		return CalculateAverageContactPoint();
+        return CalculateAverageContactPoint();
     }
 
     /// <summary>
@@ -100,7 +83,7 @@ public class HandController : MonoBehaviour
     /// <param name="z">The z.</param>
 	private Vector3 CalculateAverageContactPoint()
     {
-		float x = 0f, y = 0f, z = 0f;
+        float x = 0f, y = 0f, z = 0f;
         for (int i = 0; i < collisionContacts.Count; i++)
         {
             ContactPoint c = (ContactPoint)collisionContacts[i];
@@ -108,77 +91,61 @@ public class HandController : MonoBehaviour
             y += c.thisCollider.transform.position.y;
             z += c.thisCollider.transform.position.z;
         }
-		return new Vector3 (x, y, z) / collisionContacts.Count;
+        return new Vector3(x, y, z) / collisionContacts.Count;
     }
 
+    /// <summary>
+    /// Processes the contacts.
+    /// </summary>
     private void ProcessContacts()
     {
-        bool thumbTouch = false;
-        int othersTouch = 0;
-
+        
         if (collisionContacts.Count >= 1)
         {
-            for (int i = 0; i < bends.Length; i++)
-                bends[i] = true;
+            bool thumbTouch = false;
+            int othersTouch = 0;
 
-            ContactPoint c = (ContactPoint)collisionContacts[0];
-            for (int i = 0; i < collisionContacts.Count; i++)
-            {
-                c = (ContactPoint)collisionContacts[i];
-
-                int index = hand.GetColliders().IndexOf(c.thisCollider);
-
-                if (index < bends.Length && hand.GetManusGrab().IsGrabbing())
-                    bends[index] = false;
-
-                if (hand.GetColliders().IndexOf(c.thisCollider) == 0)
-                    thumbTouch = true;
-                else
-                    othersTouch++;
-            }
-
-            if (thumbTouch && (othersTouch >= 1))
-            {
-                hand.GetManusGrab().GrabHighlightedObject();
-            }
-            else
-            {
-                hand.GetManusGrab().DropObject();
-            }
+            ContactDetected(thumbTouch,othersTouch);
         }
-        else if (collisionContacts.Count == 0)
+        else 
         {
             hand.GetManusGrab().ClearHighlights();
         }
     }
 
     /// <summary>
-    /// If the collision contact count is greater than one the index is updated and the object is highlighted 
+    /// On contact detected, check which fingers are touching the object.
     /// </summary>
-    /// <param name="thumbTouch">if set to <c>true</c> [thumb touch].</param>
-    /// <param name="othersTouch">The others touch.</param>
-    public void ContactsCountSize(bool thumbTouch, int othersTouch)
+    /// <returns></returns>
+    private void ContactDetected(bool thumbTouch, int othersTouch)
     {
-        if (collisionContacts.Count >= 1)
-        {
-            BendFingersIsTrue();
-            ContactPoint c = (ContactPoint)collisionContacts[0];
+        ResetBends();
 
-            IndexUpdate(c, thumbTouch, othersTouch);
-            GetManusHighlightedObject(thumbTouch, othersTouch);
-        }
-        else if (collisionContacts.Count == 0)
+        ContactPoint c = (ContactPoint)collisionContacts[0];
+        for (int i = 0; i < collisionContacts.Count; i++)
         {
-            hand.GetManusGrab().ClearHighlights();
+            c = (ContactPoint)collisionContacts[i];
+
+            int index = hand.GetColliders().IndexOf(c.thisCollider);
+
+            if (index < bends.Length && hand.GetManusGrab().IsGrabbing())
+                bends[index] = false;
+
+            if (hand.GetColliders().IndexOf(c.thisCollider) == 0)
+                thumbTouch = true;
+            else
+                othersTouch++;
         }
+
+        HandleTouch(thumbTouch, othersTouch);
     }
 
     /// <summary>
-    /// if the thumb thouches the other fingers, the highligted object is picked up, else it is dropped.
+    /// Checks if the thumb touches other fingers.
     /// </summary>
     /// <param name="thumbTouch">if set to <c>true</c> [thumb touch].</param>
     /// <param name="othersTouch">The others touch.</param>
-    private void GetManusHighlightedObject(bool thumbTouch, int othersTouch)
+    private void HandleTouch(bool thumbTouch, int othersTouch)
     {
         if (thumbTouch && (othersTouch >= 1))
         {
@@ -189,62 +156,7 @@ public class HandController : MonoBehaviour
             hand.GetManusGrab().DropObject();
         }
     }
-
-    /// <summary>
-    /// Update the index of the fingers that thouch.
-    /// </summary>
-    /// <param name="c">The c.</param>
-    /// <param name="thumbTouch">if set to <c>true</c> [thumb touch].</param>
-    /// <param name="othersTouch">The others touch.</param>
-    private void IndexUpdate(ContactPoint c, bool thumbTouch, int othersTouch)
-    {
-        FingersTouch(c, thumbTouch, othersTouch);
-    }
-
-    /// <summary>
-    ///  Get the index of the fingers that thouch.
-    /// </summary>
-    /// <param name="c">The c.</param>
-    /// <param name="thumbTouch">if set to <c>true</c> [thumb touch].</param>
-    /// <param name="othersTouch">The others touch.</param>
-    private void FingersTouch(ContactPoint c, bool thumbTouch, int othersTouch)
-    {
-        for (int i = 0; i < collisionContacts.Count; i++)
-        {
-            c = (ContactPoint)collisionContacts[i];
-
-            int index = hand.GetColliders().IndexOf(c.thisCollider);
-
-            FingersNotBended(index);
-            ThumbTouched(c, thumbTouch, othersTouch);
-
-        }
-    }
-
-    /// <summary>
-    /// Verifies that the fingers are not bended.
-    /// </summary>
-    /// <param name="index">The index.</param>
-    private void FingersNotBended(int index)
-    {
-        if (index < bends.Length && hand.GetManusGrab().IsGrabbing())
-            bends[index] = false;
-    }
-
-    /// <summary>
-    /// Check if the other fingers thouch the thumb.
-    /// </summary>
-    /// <param name="c">The c.</param>
-    /// <param name="thumbTouch">if set to <c>true</c> [thumb touch].</param>
-    /// <param name="othersTouch">The others touch.</param>
-    private void ThumbTouched(ContactPoint c, bool thumbTouch,int othersTouch)
-    {
-        if (hand.GetColliders().IndexOf(c.thisCollider) == 0)
-            thumbTouch = true;
-        else
-            othersTouch++;
-    }
-
+ 
     /// <summary>
     /// Called when [collision enter].
     /// </summary>
@@ -281,10 +193,7 @@ public class HandController : MonoBehaviour
         for (int j = 0; j < hand.GetColliders().Count; j++)
         {
             if (hand.GetColliders()[j].Equals(thisCollider))
-            {
                 ContainmentCheck(col);
-            }
-
         }
     }
 
@@ -327,10 +236,7 @@ public class HandController : MonoBehaviour
         for (int j = 0; j < hand.GetColliders().Count; j++)
         {
             if (hand.GetColliders()[j].Equals(c.thisCollider))
-            {
                 collisionContacts.Remove(c);
-            }
-
         }
 
     }
